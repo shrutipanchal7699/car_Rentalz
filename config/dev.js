f53417014bd14f80bf384919351840749c184c18
Shruti Panchal <54422924+shrutipanchal7699@users.noreply.github.com>
2020-12-20 23:09:05 -0800

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+//this file is used for development
+module.exports = {
+ //   MongoDB: 'mongodb+srv://carrental:<carRentalApplication>@carrentalz.1ajb2.mongodb.net/<dbname>?retryWrites=true&w=majority'
+  //  MongoDB: 'mongodb+srv://carrental:<carRental>@carrentalz.1ajb2.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    MongoDB: 'mongodb+srv://<carRental>:<carRental>@carrentalz.1ajb2.mongodb.net/<dbname>?retryWrites=true&w=majority'
+};
